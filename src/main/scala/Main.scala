f3c3460ffdf7a718a0368ea396357ea8f01a4812
Khor Shao Liang <stephenksl@gmail.com>
2019-03-27 17:10:04 +0800

    Main.scala: add getTrajectories and filterTrajectories

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,7 +11,7 @@ import scala.reflect.ClassTag
 import java.util.Date
 import java.text.SimpleDateFormat
 
-case class DataPoint(driverID: String, orderID: String, hour: Int, minute: Int, longitude: Float, latitude: Float) extends Serializable
+case class DataPoint(driverID: String, orderID: String, minute: Int, longitude: Float, latitude: Float) extends Serializable
 
 object Main extends Main {
 
@@ -21,11 +21,15 @@ object Main extends Main {
   def main(args: Array[String]): Unit = {
     val interestLongitude = 0f
     val interestLatitude = 0f
-    val startTime = 0
-    val endTime = 0
-
-    val lines = sc.textFile("filepath")
+    val start = 9*60 + 23
+    val end = 9*60 + 23
+    val lines = sc.textFile("data/input/didi_sample_data")
     val dataPoints = rawDataPoints(lines)
+    dataPoints.saveAsTextFile("data/output")
+    val trajectories = getTrajectories(dataPoints)
+    val filteredTrajectories = filterTrajectoriesByTime(trajectories, start, end)
+
+    //filteredTrajectories.saveAsTextFile("data/output")
   }
 }
 
@@ -40,10 +44,28 @@ class Main extends Serializable {
       DataPoint(
         driverID = arr(0),
         orderID = arr(1),
-        hour = hourDf.format(arr(2)).toInt,
-        minute = minuteDf.format(arr(2)).toInt,
+        minute = (hourDf.format(arr(2).toLong).toInt * 60) + minuteDf.format(arr(2).toLong).toInt,
         longitude = arr(3).toFloat,
         latitude = arr(4).toFloat)
     })
+  }
+
+  /** Group the trajectories together */
+  def getTrajectories(dataPoints: RDD[DataPoint]): RDD[(String, Iterable[DataPoint])] = {
+    dataPoints.map(dp => (dp.orderID, dp)).groupByKey()
+  }
+
+  /** Filter the trajectories based on user input time */
+  def filterTrajectoriesByTime(dataPoints: RDD[(String, Iterable[DataPoint])], start: Int, end: Int):
+  RDD[(String, Iterable[DataPoint])] = {
+    dataPoints.filter { case (key, xs) =>
+      xs.exists { case xss =>
+        if (start <= end) {
+          (xss.minute >= start) && (xss.minute <= end)
+        } else {
+          (xss.minute >= start) && (xss.minute <= 1439) || (xss.minute >= 0) && (xss.minute <= end)
+        }
+      }
+    }
   }
 }
